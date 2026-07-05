e0f1c2ca8e5a82ade0c9041f34c9ef23ec45c723
GOGO <georgi.yazovaliiski@gmail.com>
2026-07-05 20:25:27 +0300

    imports

--- tapmymenu/src/App.js
+++ tapmymenu/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import DigitalMenu from "./DigitalMenu";
 
